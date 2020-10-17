fb5c484fc0be7b5ff20e2fff53cf985b76586e53
Phi Hung <phihunds1@gmail.com>
2020-10-17 18:04:37 +0700

    them username va pass

--- dangnhap.js
+++ dangnhap.js
@@ -1,3 +1,11 @@
+
 function kiemtra() {
-    
+    let username = document.getElementById("username").value
+    console.log("đang kiểm tra")
+    if (username == "Admin") {
+        let password = prompt("Hãy nhập mật khẩu")
+        if (password == "TheMaster") {
+            alert("Welcome")
+        }
+    }
 }
